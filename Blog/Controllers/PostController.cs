c451a7ad498c3777f71af3be8bf3e2802f0a157f
unknown <andrechavesg@gmail.com>
2018-09-22 13:58:11 -0700

    exercicios 3.10

--- Blog/Controllers/PostController.cs
+++ Blog/Controllers/PostController.cs
@@ -10,19 +10,6 @@ namespace Blog.Controllers
 {
     public class PostController : Controller
     {
-        private IList<Post> lista;
-
-        public PostController()
-        {
-            lista = new List<Post>()
-            {
-                new Post(){ Titulo = "Harry Potter 1",Resumo = "Pedra Filosofal",Categoria = "Filme, Livro" },
-                new Post(){ Titulo = "CassinoRoyale",Resumo = "007",Categoria = "Filme" },
-                new Post(){ Titulo = "Monge e o Executivo",Resumo = "Romance sobre Liderança",Categoria = "Livro" },
-                new Post(){ Titulo = "New York, New York",Resumo = "Sucesso de Frank Sinatra",Categoria = "Música" }
-            };
-        }
-
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -33,15 +20,16 @@ namespace Blog.Controllers
         }
 
         public IActionResult Novo()
-        {
+        { 
             return View();
         }
 
         [HttpPost]
         public IActionResult Adiciona(Post post){
-            lista.Add(post);
+            PostDAO dao = new PostDAO();
+            dao.Adiciona(post);
 
-            return View("Index", lista);
+            return RedirectToAction("Index");
         }
     }
 }
