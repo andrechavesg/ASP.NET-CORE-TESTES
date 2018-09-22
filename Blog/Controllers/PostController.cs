eb1ace40ae589aa85c3b4e63a169abe3f1ce21e9
André Chaves <andrechavesg@gmail.com>
2018-09-22 14:49:07 -0300

    exercicios 1.11

---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var listaDePosts = new List<Post>()
+            {
+                new Post(){ Titulo = "Harry Potter 1",Resumo = "Pedra Filosofal",Categoria = "Filme, Livro"},
+                new Post(){ Titulo = "CassinoRoyale",Resumo = "007",Categoria = "Filme"},
+                new Post(){ Titulo = "Monge e o Executivo",Resumo = "Romance sobre Liderança",Categoria = "Livro"},
+                new Post(){ Titulo = "New York, New York",Resumo = "Sucesso de Frank Sinatra",Categoria = "Música"}
+            };
+
+            ViewBag.Posts = listaDePosts;
+
+            return View();
+        }
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Blog.Models
+{
+    public class Post
+    {
+        public String Titulo
+        {
+            get;
+            set;
+        }
+
+        public String Resumo
+        {
+            get;
+            set;
+        }   
+
+        public String Categoria
+        {
+            get;
+            set;
+        }
+
+        public Post()
+        {
+        }
+    }
+}
