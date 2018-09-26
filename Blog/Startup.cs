ce85188d7ccfe0d2f24e37bffebae3e2a4059534
unknown <andrechavesg@gmail.com>
2018-09-26 05:51:21 -0700

    exercicios 8.8

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -55,6 +55,9 @@ namespace Blog
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Post}/{action=Index}/{id?}");
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
