d112c8e9327ba1135b4b4b00d2c09f7ad8cafe4c
unknown <andrechavesg@gmail.com>
2018-09-27 03:38:59 -0700

    exercicios completos

---
+++ Blog/Areas/API/Controllers/PostController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.DAO;
+using Blog.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Areas.API.Controllers
+{
+    [Route("api/post")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly PostDAO dao;
+
+        public PostController(PostDAO dao)
+        {
+            this.dao = dao;
+        }
+
+        // GET: api/Post
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IList<Post> posts = dao.Lista();
+
+            return Ok(posts);
+        }
+
+        // GET: api/Post/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            Post post = dao.BuscaPorId(id);
+            return Ok(post);
+        }
+
+        // POST: api/Post
+        [HttpPost]
+        public IActionResult Post([FromBody] Post post)
+        {
+            dao.Adiciona(post);
+            return CreatedAtAction("BuscaPostPorId", new { id = post.Id }, post);
+        }
+
+        // PUT: api/Post/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Post post)
+        {
+            Post postBanco = dao.BuscaPorId(id);
+            if(postBanco == null)
+            {
+                return NotFound();
+            }
+            postBanco.Titulo = post.Titulo;
+            postBanco.Resumo = post.Resumo;
+            postBanco.Categoria = post.Categoria;
+            postBanco.DataPublicacao = post.DataPublicacao;
+            dao.Atualiza(postBanco);
+            return NoContent();
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if(dao.BuscaPorId(id) == null)
+            {
+                return NotFound();
+            }
+            dao.Remove(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Blog/Areas/API/Controllers/PostsGeradoController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Blog.Infra;
+using Blog.Models;
+
+namespace Blog.Areas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsGeradoController : ControllerBase
+    {
+        private readonly BlogContext _context;
+
+        public PostsGeradoController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PostsGerado
+        [HttpGet]
+        public IEnumerable<Post> GetPosts()
+        {
+            return _context.Posts;
+        }
+
+        // GET: api/PostsGerado/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPost([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(post);
+        }
+
+        // PUT: api/PostsGerado/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPost([FromRoute] int id, [FromBody] Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(post).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PostsGerado
+        [HttpPost]
+        public async Task<IActionResult> PostPost([FromBody] Post post)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPost", new { id = post.Id }, post);
+        }
+
+        // DELETE: api/PostsGerado/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePost([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+
+            return Ok(post);
+        }
+
+        private bool PostExists(int id)
+        {
+            return _context.Posts.Any(e => e.Id == id);
+        }
+    }
+}
